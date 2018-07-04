f8c4e1ce74925dfff0ae1b21f1e1e4ef5a661e5e
Shalu Singhal <ssshalusinghal@gmail.com>
2018-07-04 12:03:19 +0530

    Feat: tweetstream to component

--- client/src/components/main/containers/TweetStream.js
+++ client/src/components/main/containers/TweetStream.js
@@ -1,26 +1,36 @@
-import React from 'react';
+import { Component } from 'react';
 import { connect } from 'react-redux';
 import { newTweet } from '../../../actions';
+
 import io from 'socket.io-client';
 
-const TweetStream = ({ dispatch }) => {
-    const socket = io('http://localhost:8000', { transports: ['websocket', 'polling'] });
+class TweetStream extends Component {
+    componentDidMount () {
+        const socket = io('http://localhost:8000', { transports: ['websocket', 'polling'] });
 
-    socket.on('connect', () => {
-        console.log('Socket Connected');
+        socket.on('connect', () => {
+            console.log('Socket Connected');
 
-        socket.on('tweets', (data) => {
-            dispatch(newTweet(data));
+            socket.on('tweets', (data) => {
+                this.props.newTweet(data);
+            });
         });
-    });
 
-    socket.on('disconnect', () => {
-        socket.off('tweets');
-        socket.removeAllListeners('tweets');
-        console.log('Socket Disconnected');
-    });
+        socket.on('disconnect', () => {
+            socket.off('tweets');
+            socket.removeAllListeners('tweets');
+            console.log('Socket Disconnected');
+        });
+    }
 
-    return (<div></div>);
+    render () {
+        return null;
+    }
 }
 
-export default connect()(TweetStream);
+const mapDispatchToProps = {
+    newTweet
+};
+
+
+export default connect(null, mapDispatchToProps)(TweetStream);
